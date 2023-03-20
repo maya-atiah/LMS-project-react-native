8d738243a674049f9a681c8b4d4a68b7a1754f04
Maya <maya.atiah.99@outlook.com>
2023-03-21 01:19:19 +0200

    attendance sheet is almost finished

--- components/Attendance.jsx
+++ components/Attendance.jsx
@@ -4,20 +4,16 @@ import { StyleSheet } from "react-native";
 import { SelectList } from "react-native-dropdown-select-list";
 import { useState } from "react";
 import { useEffect } from "react";
+import RadioGroup from "react-native-radio-buttons-group";
 import axios from "axios";
+import { ScrollView } from "react-native";
 
 function Attendance() {
-
-
   const [grade, setGrade] = useState([]);
   const [section, setSection] = useState([]);
   const [selectedGrade, setSelectedGrade] = useState(null);
   const [selectedSection, setSelectedSection] = useState(null);
   const [student, setStudent] = useState([]);
-  const [gradeId, setGradeId] = useState([]);
-  const [sectionId, setSectionId] = useState([]);
-
-
 
   const fetchGradeSection = async () => {
     await axios
@@ -26,97 +22,135 @@ function Attendance() {
       .catch((err) => console.log(err));
   };
 
-
-
   const fetchallStudentByGradeSection = async () => {
-    
     await axios
-      .get(`http://192.168.1.114:8000/api/allStudent/${selectedGrade?.id}/${selectedSection?.id}`)
+      .get(
+        `http://192.168.1.114:8000/api/allStudent/${selectedGrade?.id}/${selectedSection?.id}`
+      )
       .then((res) => setStudent(res.data))
       .catch((err) => console.log(err));
   };
-
- console.log('student',student);
 
   useEffect(() => {
     fetchGradeSection();
   }, []);
 
   useEffect(() => {
-  console.log("selectgradeid", selectedGrade);
-  console.log("selectsectionid", selectedSection);
-   if(selectedGrade && selectedSection )
-   {
-     fetchallStudentByGradeSection();
-      }
-}, [selectedGrade, selectedSection])
-
-
-
-
+    if (selectedGrade && selectedSection) {
+      fetchallStudentByGradeSection();
+    }
+  }, [selectedGrade, selectedSection]);
 
   const handleGradeChange = (value) => {
-    
     const selected = grade.find((grade) => grade.name === value);
-    
     setSection(selected.sections);
     setSelectedGrade(selected);
   };
-  
+
+  const [radioButtons, setRadioButtons] = useState([
+    {
+      id: "1", // acts as primary key, should be unique and non-empty string
+      label: "present",
+      value: "present",
+    },
+    {
+      id: "2",
+      label: "absent",
+      value: "absent",
+    },
+    {
+      id: "3",
+      label: "late",
+      value: "late",
+    },
+  ]);
+
+  function onPressRadioButton(radioButtonsArray) {
+    setRadioButtons(radioButtonsArray);
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Attendance</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Attendance</Text>
 
-      <SelectList
-        setSelected={handleGradeChange}
-        data={grade.map((g) => g.name)}
-        placeholder={"Select Grade"}
-        boxStyles={{ backgroundColor: "#EE8B3A", marginHorizontal: 20 }}
-        dropdownStyles={{ backgroundColor: "#EE8B3A", color: "white" }}
-        inputStyles={{ color: "white" }}
-      
-      />
+        <SelectList
+          setSelected={handleGradeChange}
+          data={grade.map((g) => g.name)}
+          placeholder={"Select Grade"}
+          boxStyles={{ backgroundColor: "#EE8B3A", marginHorizontal: 20 }}
+          dropdownStyles={{ backgroundColor: "#EE8B3A", color: "white" }}
+          inputStyles={{ color: "white" }}
+        />
 
-      <SelectList
-        setSelected={(value) => {
-          const selected = section.find((s) => s.letter === value);
-          setSelectedSection(selected);
-         
-        }}
-        data={section.map((s) => s.letter)}
-        placeholder={"Select Section"}
-        boxStyles={{
-          backgroundColor: "#EE8B3A",
-          marginVertical: 10,
-          marginHorizontal: 20,
-        }}
-        dropdownStyles={{ backgroundColor: "#EE8B3A", color: "white" }}
-        inputStyles={{ color: "white" }}
-        
-      />
-      {selectedGrade && selectedSection &&
-        <Text style={styles.titleGrade}>
-          {selectedGrade?.id} {selectedSection?.id}
-        </Text>}
-      {student &&
-        student.map((student) => {
-          return <Text key={student.id}>{student.firstName}</Text>;
-        })}
-    </View>
+        <SelectList
+          setSelected={(value) => {
+            const selected = section.find((s) => s.letter === value);
+            setSelectedSection(selected);
+          }}
+          data={section.map((s) => s.letter)}
+          placeholder={"Select Section"}
+          boxStyles={{
+            backgroundColor: "#EE8B3A",
+            marginVertical: 10,
+            marginHorizontal: 20,
+          }}
+          dropdownStyles={{ backgroundColor: "#EE8B3A", color: "white" }}
+          inputStyles={{ color: "white" }}
+        />
+        {selectedGrade && selectedSection && (
+          <Text style={styles.titleGrade}>
+            {selectedGrade.name} {selectedSection.letter}
+          </Text>
+        )}
+        {student &&
+          student.map((s) => {
+            return (
+              <View style={styles.form}>
+                <Text style={styles.titlename}>
+                  {s.firstName} {s.lastName}
+                </Text>
+                <RadioGroup
+                  style={styles.titlename}
+                  radioButtons={radioButtons}
+                  onPress={onPressRadioButton}
+                />
+              </View>
+            );
+          })}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 100,
+    paddingTop: 50,
     paddingHorizontal: 20,
+
   },
   title: {
     fontSize: 30,
     color: "#1e90ff",
   },
   titleGrade: {
+    fontSize: 20,
+    color: "#1e90ff",
+  },
+  form: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderColor: "#EE8B3A",
+    width: "85%",
+    borderWidth: 3,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  titlename: {
     fontSize: 20,
     color: "#1e90ff",
   },
