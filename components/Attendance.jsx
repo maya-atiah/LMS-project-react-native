87425adb76595b541f99d9adfc1b832f950eda4b
Maya <maya.atiah.99@outlook.com>
2023-03-20 23:07:38 +0200

    attendance fetching grades

--- components/Attendance.jsx
+++ components/Attendance.jsx
@@ -9,15 +9,13 @@ import axios from "axios";
 function Attendance() {
 
 
-
   const [grade, setGrade] = useState([]);
   const [section, setSection] = useState([]);
   const [selectedGrade, setSelectedGrade] = useState(null);
   const [selectedSection, setSelectedSection] = useState(null);
   const [student, setStudent] = useState([]);
-  const [gradeId, setGradeId] = useState(null);
-  const [sectionId, setSectionId] = useState(null);
-
+  const [gradeId, setGradeId] = useState([]);
+  const [sectionId, setSectionId] = useState([]);
 
 
 
@@ -27,26 +25,25 @@ function Attendance() {
       .then((res) => setGrade(res.data))
       .catch((err) => console.log(err));
   };
-  console.log("data", grade);
 
 
 
-
-  const fetchallStudentByGradeSection = async (gradeId, sectionId) => {
+  const fetchallStudentByGradeSection = async () => {
+    console.log("selectgradeid", selectedGrade?.id);
     await axios
-      .get(`http://192.168.1.114:8000/api/allStudent/${gradeId}/${sectionId}`)
-      .then((res) => {
-        setStudent(res.data);
-        setTable(true);
-      })
+      .get(`http://192.168.1.114:8000/api/allStudent/${selectedGrade?.id}/${selectedSection?.id}`)
+      .then((res) => setStudent(res.data))
       .catch((err) => console.log(err));
   };
 
-
+ console.log(student);
 
   useEffect(() => {
     fetchGradeSection();
+    fetchallStudentByGradeSection();
   }, []);
+
+
 
 
 
@@ -54,30 +51,31 @@ function Attendance() {
     setSelectedGrade(value);
     const selected = grade.find((grade) => grade.name === value);
     setSection(selected.sections);
-    setGradeId(selected.id); 
-     };
+    console.log('selectedGrade',selectedGrade)
+  };
+  
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Attendance</Text>
 
-    
       <SelectList
         setSelected={handleGradeChange}
-        data={grade ? grade.map((grade) => grade.name) : []}
+        data={grade.map((g) => g.name)}
         placeholder={"Select Grade"}
         boxStyles={{ backgroundColor: "#EE8B3A", marginHorizontal: 20 }}
         dropdownStyles={{ backgroundColor: "#EE8B3A", color: "white" }}
         inputStyles={{ color: "white" }}
-        onChange={() => {
-          setSection(grade.sections);
-          setGradeId(grade.id);
-          
-        }}
+      
       />
+
       <SelectList
-        setSelected={setSelectedSection}
-        data={section.map((section) => section.letter)}
+        setSelected={(value) => {
+          const selected = section.find((s) => s.letter === value);
+          setSelectedSection(selected);
+          console.log("selectedGrade", selectedGrade);
+        }}
+        data={section.map((s) => s.letter)}
         placeholder={"Select Section"}
         boxStyles={{
           backgroundColor: "#EE8B3A",
@@ -86,21 +84,16 @@ function Attendance() {
         }}
         dropdownStyles={{ backgroundColor: "#EE8B3A", color: "white" }}
         inputStyles={{ color: "white" }}
-        onChange={(value) => {
-          const selected = section.find((s) => s.letter === value);
-          setSelectedSection(selected);
-          setSectionId(selected.id);
-
-        }}
+        
       />
-    
+
       <Text style={styles.titleGrade}>
-        {selectedGrade} {selectedSection}
+        {selectedGrade?.id} {selectedSection?.id}
       </Text>
-      <Text style={styles.titleGrade}>
-       
-        {gradeId}{sectionId}
-      </Text>
+      {student &&
+        student.map((student) => {
+          return <Text key={student.id}>{student.firstName}</Text>;
+        })}
     </View>
   );
 }
