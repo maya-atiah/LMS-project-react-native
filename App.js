d16ba69e1167110e08568319662cd5b014435d31
Maya <maya.atiah.99@outlook.com>
2023-03-21 14:42:30 +0200

    fixing errors

--- App.js
+++ App.js
@@ -1,30 +1,38 @@
 // import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
-import Attendance from './components/Attendance';
-// import Login from "./components/login";
+import Login from "./components/login";
+import Attendance from "./components/Attendance";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
+const Stack = createStackNavigator();
 
 
 export default function App() {
+
   return (
-    <View >
-      {/* <Text>Hi, I am Assala  </Text>
-      <StatusBar style="auto" /> */}
-      {/* <Login/> */}
-      <Attendance/>
-    </View>
+    // <View  style={styles.background}>
+    //   <Login/>
+    // </View>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Attendance" component={Attendance} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
-
-
--- components/login.jsx
+++ components/login.jsx
@@ -1,129 +1,149 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { ImageBackground, StyleSheet, View, Image, Text, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+import Attendance from "./Attendance";
 
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-const Login = () => {
-    const [email, setEmail]= useState("");
-    const [Password, setPassword]= useState("");
-    const[errorMessage, setErrorMessage]=useState("");
+  const handleEmailChange = (text) => {
+    setEmail(text);
+  };
 
-    const handleEmailChange = (text) => {
-        setEmail(text);
-    };
+  const handlePasswordChange = (text) => {
+    setPassword(text);
+  };
 
-    const handlePasswordChange = (text) => {
-        setPassword(text);
-    };
-
-    const handleSubmit = async () => {
-        try{
-            const response = await fetch("http://192.168.0.113:8000/api/user/login ", {
-                method: "POST",
-                headers:{
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    email,
-                    Password,
-                }),
-            });
-            const data = await response.json();
-            {console.log(data)}
-            if(response.ok){
-                Alert.alert("Login successful", "", [{
-                    text: "ok",
-                    onPress: () => {
-                        window.localStorage.setItem("token", data.token);
-                    }
-                },
-            ]);
-            }else{
-                Alert.alert("login failed", data.message);
-            }
-        } catch (error){
-            console.error(error);
-            Alert.alert("Internet error")
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.43.159:8000/api/user/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
         }
-    };
+      );
+      const data = await response.json();
+      {
+        console.log(data);
+      }
+      if (response) {
+        console.log("meshyeeee");
+        Alert.alert("Login successful", "", [
+          {
+            text: "ok",
+            onPress: () => {
+              //   window.localStorage.setItem("token", data.token);
+              navigation.navigate("Attendance");
+            },
+          },
+        ]);
+      } else {
+        Alert.alert("login failed", data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert("errorrrr");
+    }
+  };
 
-    useEffect(() => {
-        if(errorMessage){
-            const timeoutId = setTimeout(() =>{
-                setErrorMessage("");
-            }, 2000);
-            return () => clearTimeout(timeoutId);
-        }
-    },[errorMessage]);
+  useEffect(() => {
+    if (errorMessage) {
+      const timeoutId = setTimeout(() => {
+        setErrorMessage("");
+      }, 2000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [errorMessage]);
   return (
-   
+    <View style={styles.containerrr}>
+      <Image source={require("../assets/logo.png")} style={styles.imagelogin} />
 
-
-    <View style={styles.container}>
-<Image
-       source={require('../assets/logo.png')}
-       style={{width: 360, height: 200}}
-  
-/>
-
-     <Text style={styles.title}>Login</Text>
-    <TextInput style={styles.input} placeholder="Enter your Email" onChangeText={handleEmailChange}></TextInput>
-    <TextInput style={styles.input} placeholder="Enter your Password" onChangeText={handlePasswordChange}></TextInput>
-    <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-    <Text style={styles.buttonText}>Login</Text>
-    </TouchableOpacity>
-        </View>
+      <Text style={styles.title}>Welcome back</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Enter your Email'
+        onChangeText={handleEmailChange}
+      ></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder='Enter your Password'
+        onChangeText={handlePasswordChange}
+      ></TextInput>
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#017f94',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 400,
-     
-    },
+  containerrr: {
+    flex: 1,
+    backgroundColor: "#017f94",
+    alignItems: "center",
+    paddingTop: 150,
+    width: "100%",
+  },
 
-    title:{
-        color: '#EE8B3A',
-        fontSize: 35,
-    //  marginBottom: 10,
-     fontWeight: 100,
-    },
-    
-    input:{
-        marginTop: 0,
-        width: '80%',
-        height: 45,
-        margin: 10,
-        padding: 10,
-        borderColor: '#EE8B3A',
-        borderRadius: 5,
-        borderWidth: 2,
-        color: 'black',
-    },
+  title: {
+    color: "#EE8B3A",
+    fontSize: 35,
+    marginBottom: 40,
+    height: 50,
+  },
 
-    button:{
-        width: '40%',
-        height: 50,
-        marginTop: 10,
-        backgroundColor: '#EE8B3A',
-        borderRadius: 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-        
-    },
-    buttonText:{
-        color: '#fff',
-        fontSize: 18,
+  imagelogin: {
+    backgroundColor: "#017f94",
+    width: 360,
+    height: 200,
+  },
 
-    }
+  input: {
+    marginTop: 0,
+    width: 300,
+    height: 45,
+    margin: 10,
+    padding: 10,
+    borderColor: "#EE8B3A",
+    borderRadius: 5,
+    borderWidth: 2,
+    color: "#fff",
+  },
 
+  button: {
+    width: 150,
+    height: 50,
+    marginTop: 10,
+    backgroundColor: "#EE8B3A",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-
-
-  })
-
+  buttonText: {
+    color: "#fff",
+    fontSize: 25,
+  },
+});
 export default Login;
