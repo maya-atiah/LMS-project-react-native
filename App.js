4d4efaf73d2b723438785428d5880672fc853c76
Assala <ismailassala1@gmail.com>
2023-03-21 10:50:35 +0200

    navigationn

--- App.js
+++ App.js
@@ -12,15 +12,6 @@ const Stack = createStackNavigator();
 
 export default function App() {
 
-  // const [loggedIn, setLoggedIn] = useState(false);
-
-  // const handleLogin = () => {
-  //   setLoggedIn(true);
-  // };
-
-  // const handleLogout = () => {
-  //   setLoggedIn(false);
-  // };
   return (
     // <View  style={styles.background}>
     //   <Login/>
@@ -32,19 +23,7 @@ export default function App() {
         <Stack.Screen name="Attendance" component={Attendance} />
       </Stack.Navigator>
     </NavigationContainer>
-  //   <NavigationContainer>
-  //   <Stack.Navigator>
-  //     {!loggedIn ? (
-  //       <Stack.Screen name="Login">
-  //         {(props) => <Login {...props} onLogin={handleLogin} />}
-  //       </Stack.Screen>
-  //     ) : (
-  //       <Stack.Screen name="Attendance">
-  //         {(props) => <Attendance {...props} onLogout={handleLogout} />}
-  //       </Stack.Screen>
-  //     )}
-  //   </Stack.Navigator>
-  // </NavigationContainer>
+
   );
 }
 
